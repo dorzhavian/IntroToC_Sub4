13431f0bed54b0d1d9ac197f23903002e0cfda7d
Dor Zhavian <dorzhavian@gmail.com>
2025-01-29 19:00:36 +0200

    Add project files.

---
+++ SuperFile.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <stdio.h>
+#include "Supermarket.h"
+#include "Customer.h"
+
+int		saveSuperMarketToFile(const SuperMarket* pMarket, const char* fileName,
+	const char* customersFileName);
+int		loadSuperMarketFromFile(SuperMarket* pMarket, const char* fileName,
+	const char* customersFileName);
+int		saveCustomersToTextFile(const Customer* customerArr, int customerCount,
+	const char* customersFileName);
+Customer* loadCustomersFromTextFile(const char* customersFileName, int* pCount);
+void		freeCustomerCloseFile(Customer* customerArr, int customerIndex, FILE* fp);
+
